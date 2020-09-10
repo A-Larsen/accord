00e5a6549afc76a0ab359845b76652d443467716
A-Larsen <austinlarsengit@gmail.com>
2020-09-10 01:06:25 -0400

    mobile

--- public/websockets.js
+++ public/websockets.js
@@ -393,13 +393,18 @@ popup_mobile_createMessage.style.backgroundColor = "#474b53";
 popup_mobile_createMessage.innerHTML = 
 "<h1>CREATE MESSAGE</h1><br><br>"+
 "<br><textarea cols='30' rows='5' type='text'></textarea><br>"+
-"<button>add friend</button>&nbsp;<button>cancel</button><br>";
+"<button>cancel</button>&nbsp;<button>send</button><br>";
 
 popup_mobile_createMessage.getElementsByTagName('button')[0].style.width = "45%";
 popup_mobile_createMessage.getElementsByTagName('button')[0].style.marginRight = "8%";
 popup_mobile_createMessage.getElementsByTagName('button')[0].style.height = "5%";
+popup_mobile_createMessage.getElementsByTagName('button')[0].style.fontSize = "4vw";
 popup_mobile_createMessage.getElementsByTagName('button')[1].style.width = "45%";
 popup_mobile_createMessage.getElementsByTagName('button')[1].style.height = "5%";
+popup_mobile_createMessage.getElementsByTagName('button')[1].style.fontSize = "4vw";
+popup_mobile_createMessage.getElementsByTagName('textarea')[0].style.fontSize = "4vw";
+popup_mobile_createMessage.getElementsByTagName('textarea')[0].style.height = "70%";
+popup_mobile_createMessage.getElementsByTagName('textarea')[0].style.width = "100%";
 
 popup_mobile_createMessage.getElementsByTagName('button')[1].onclick = function(){
 	document.body.removeChild(popup_mobile_createMessage);
@@ -423,6 +428,7 @@ function initPage(){
 
 		el_msg.onclick = function() {
 			document.body.appendChild(popup_mobile_createMessage);
+			popup_mobile_createMessage.getElementsByTagName('textarea')[0].focus();
 		}
 	}
 } 
