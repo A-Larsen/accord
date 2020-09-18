7446e4cc437725ec6248d7ed75f4f96108880b0d
A-Larsen <austinlarsengit@gmail.com>
2020-09-18 13:40:13 -0400

    file cleanup

--- cleardbs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sqlite3 server/users.db "DELETE FROM users;" 
-
-if [ -f "/server/chatrooms.db" ]; then
-	doas rm -r server/chatrooms.db 
-fi
--- mem.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MEM_
-#define _MEM_
-#include <malloc.h>
-
-void ** dynamicArrayResize(void **data, size_t nmemb, size_t size);
-void * dynamicArrayResize2(void *data, size_t nmemb, size_t size);
-
-#endif 
